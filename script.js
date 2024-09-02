8243c90bfdd16f7c4d9bd0ffc60446ebe6e9d0fd
HemaCSE <144872077+HemaCSE@users.noreply.github.com>
2024-09-02 13:05:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function hema(){
+
+    const colors =["#FFFFFF","#000000"];
+    const randomColor=colors[Math.floor(Math.random()*colors.length)];
+    document.body.style.backgroundColor=randomColor;
+}
